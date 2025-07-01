bf9b1c1c4ed60ea50c2c8c4760bb9bc3de3e271d
smbaq <mysarahsb@gmail.com>
2025-07-01 05:10:42 +0100

    Add horizontal triangle movement using uniform in shader
    Added GLM include directory to project settings and included GLM in main.cpp. Introduced a uniform variable 'xMove' in the vertex shader and updated the main loop to animate the triangle horizontally by updating this uniform each frame.

--- Baqader_GameEngine/main.cpp
+++ Baqader_GameEngine/main.cpp
@@ -2,11 +2,23 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <string.h>
+#include <cmath>
+#include <glm/mat4x4.hpp>
+#include "glm/gtc/matrix_transform.hpp"
+#include <glm/gtc/type_ptr.hpp>
+
+
 
 //window dimensions
-GLint WIDTH = 800, HEIGHT = 600;
+const GLint WIDTH = 800, HEIGHT = 600;
+const float toRadians = 3.1459265f / 180.0f;
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, shader, uniformModel;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.7f;
+float triIncrement = 0.0005f;
 
 //vertex shader
 static const char* vShader = R"(
@@ -14,9 +26,11 @@ static const char* vShader = R"(
 
 layout (location = 0) in vec3 pos;
 
+uniform mat4 model;
+
 void main()
 {
-	gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+	gl_Position = model * vec4(pos.x, pos.y, pos.z, 1.0);
 }
 )";
 
@@ -115,6 +129,9 @@ void CompileShader() {
 		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
 		return;
 	}
+
+	uniformModel = glGetUniformLocation(shader, "model");
+
 }
 
 int main()
@@ -181,20 +198,40 @@ int main()
 		//process + handle inputs
 		glfwPollEvents();
 
+		if (direction)
+		{
+			triOffset += triIncrement;
+		}
+		else
+		{
+			triOffset -= triIncrement;
+		}
+
+		if (abs(triOffset) >= triMaxOffset)
+		{
+			direction = !direction;
+		}
+
 		//render
 		glUseProgram(shader);
+
+		glm::mat4 model(1.0f);
+		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+
+		//glUniform1f(uniformModel, triOffset);
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glBindVertexArray(0);
 		glUseProgram(0);
 
-
-
 		//swap buffers
 		glfwSwapBuffers(window);
 
 	}
+
 
 	glfwTerminate();
 	return 0;
